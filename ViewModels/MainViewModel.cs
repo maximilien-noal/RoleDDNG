ef0644c03fa7d69b7ca92b3de27a5f3acab2d29c
Maximilien Noal <noal.maximilien@gmail.com>
2019-11-12 21:40:47 +0100

    Full main window menu
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
+using System.Windows;
 
 namespace RoleDDNg.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+#pragma warning disable CA1822 // Static bindings work, but makes the designer view throw an error.
+
+        public RelayCommand ExitAppCommand { get => new RelayCommand(() => { Application.Current.Dispatcher.Invoke(() => Application.Current.MainWindow.Close()); }); }
+#pragma warning restore CA1822
     }
 }
