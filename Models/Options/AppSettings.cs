d62f6e1c9430a608d554f70b9aa8b3664f3ac159
Maximilien Noal <noal.maximilien@gmail.com>
2019-11-25 23:05:06 +0100

    AppSettings
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- Models/Options/AppSettings.cs
+++ Models/Options/AppSettings.cs
@@ -1,10 +1,23 @@
 ﻿using GalaSoft.MvvmLight;
+using System;
 
 namespace RoleDDNG.Models.Options
 {
+    /// <summary>
+    /// UserSecrets in %WINDIR%/ROLE.INI
+    /// </summary>
     public class AppSettings : ObservableObject
     {
-        private string _mainWindowPlacement = "";
-        public string MainWindowPlacement { get => _mainWindowPlacement; set { Set(nameof(MainWindowPlacement), ref _mainWindowPlacement, value); } }
+        private string _mainWindowX = "";
+
+        public string MainWindowX { get => _mainWindowX; set { Set(nameof(MainWindowX), ref _mainWindowX, value); } }
+
+        private string _mainWindowY = "";
+
+        public string MainWindowY { get => _mainWindowY; set { Set(nameof(MainWindowY), ref _mainWindowY, value); } }
+
+        private Uri _bdd = default;
+
+        public Uri Bdd { get => _bdd; set { Set(nameof(_bdd), ref _bdd, value); } }
     }
 }
