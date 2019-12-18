cf324b08886360c90c0c8487c2a712ba8849a99f
Maximilien Noal <noal.maximilien@gmail.com>
2019-12-18 20:17:24 +0100

    Really async (De)Serialization
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- DataAccess/ModelSerializer.cs
+++ DataAccess/ModelSerializer.cs
@@ -1,36 +1,36 @@
-﻿using RoleDDNG.Interfaces.Serialization;
-
-using System.IO;
+﻿using System.IO;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+
+using RoleDDNG.Interfaces.Serialization;
 
 namespace DataAccess
 {
     public class ModelSerializer<T> : IAsyncSerializer<T>
     {
-        public Task<T> DeserializeAsync(string filePath)
+        public async Task<T> DeserializeAsync(string filePath)
         {
-            return Task.Run<T>(() =>
+            T deserializedMdodel = await Task.Run(() =>
             {
                 XmlReaderSettings settings = new XmlReaderSettings() { XmlResolver = null };
                 using XmlReader reader = XmlReader.Create(filePath, settings);
                 var serializer = new XmlSerializer(typeof(T));
                 return (T)serializer.Deserialize(reader);
-            });
+            }).ConfigureAwait(true);
+            return deserializedMdodel;
         }
 
-        public Task SerializeAsync(string filePath, T objectToSerialize)
+        public async Task SerializeAsync(string filePath, T objectToSerialize)
         {
-            return Task.Run(() =>
+            await Task.Run(() =>
             {
                 XmlSerializer serializer = new XmlSerializer(typeof(T));
                 using var writer = new StreamWriter(filePath, false, Encoding.Unicode);
                 serializer.Serialize(writer, objectToSerialize);
                 writer.Close();
-            });
+            }).ConfigureAwait(true);
         }
     }
 }
