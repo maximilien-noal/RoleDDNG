20212c72f935931883a7e7bc52286acf791c1840
Maximilien Noal <noal.maximilien@gmail.com>
2019-12-25 10:01:45 +0100

    Enumerable to visibility converter
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ Role/Converters/EnumerableToVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RoleDDNG.Role.Converters
+{
+    public class EnumerableToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return Visibility.Collapsed;
+            }
+            if (value is IEnumerable == false)
+            {
+                return Visibility.Visible;
+            }
+            var count = 0;
+            foreach (var item in (IEnumerable)value)
+            {
+                count = 1;
+                break;
+            }
+            if (count > 0)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Array.Empty<object>();
+        }
+    }
+}
