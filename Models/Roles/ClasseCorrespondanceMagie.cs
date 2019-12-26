b258d6aa760f87873653d4e5d3e8132d6e2f3ca2
Maximilien Noal <noal.maximilien@gmail.com>
2019-12-26 21:24:23 +0100

    Models Roles : ClasseCorrespondanceMagie
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ Models/Roles/ClasseCorrespondanceMagie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace RoleDDNG.Models.Roles
+{
+    public class ClasseCorrespondanceMagie : ObservableObject
+    {
+        private string _classeEsclave = "";
+
+        public string ClasseEsclave { get => _classeEsclave; set { Set(nameof(ClasseEsclave), ref _classeEsclave, value); } }
+
+        private int _niveau = 0;
+
+        public int Niveau { get => _niveau; set { Set(nameof(Niveau), ref _niveau, value); } }
+
+        private string _classeMaitre = "";
+
+        public string ClasseMaitre { get => _classeMaitre; set { Set(nameof(ClasseMaitre), ref _classeMaitre, value); } }
+
+        private int _point = 0;
+
+        public int Point { get => _point; set { Set(nameof(Point), ref _point, value); } }
+
+        private int _bonusDeSort = 0;
+
+        public int BonusDeSort { get => _bonusDeSort; set { Set(nameof(BonusDeSort), ref _bonusDeSort, value); } }
+
+        private int _sortAutreEnPlus = 0;
+
+        public int SortAutreEnPlus { get => _sortAutreEnPlus; set { Set(nameof(SortAutreEnPlus), ref _sortAutreEnPlus, value); } }
+    }
+}
