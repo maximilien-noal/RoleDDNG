3fd4a1fd0b820fdff0e4aa814b7be9e46057b7b6
Maximilien Noal <noal.maximilien@gmail.com>
2020-06-18 13:55:49 +0200

    Town Generator custom icon
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- Role/UserControls/TownGeneratorUserControl.xaml.cs
+++ Role/UserControls/TownGeneratorUserControl.xaml.cs
@@ -1,18 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using RoleDDNG.Role.UserControls.Traits;
+using System.Windows.Controls;
 
 namespace RoleDDNG.Role.UserControls
 {
     /// <summary>
     /// Interaction logic for TownGeneratorUserControl.xaml.
     /// </summary>
-    public partial class TownGeneratorUserControl : UserControl
+    public partial class TownGeneratorUserControl : UserControl, IUpdateMdiWindowIconTrait
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TownGeneratorUserControl" /> class.
-        /// </summary>
         public TownGeneratorUserControl()
         {
             this.InitializeComponent();
+            this.Loaded += (s, e) => ((IUpdateMdiWindowIconTrait)this).ChangeMdiWindowIcon(this);
         }
     }
 }
--- Role/UserControls/Traits/IUpdateMdiWindowIconTrait.cs
+++ Role/UserControls/Traits/IUpdateMdiWindowIconTrait.cs
@@ -1,8 +1,6 @@
 ﻿using Hammer.MDI.Control;
-using System;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace RoleDDNG.Role.UserControls.Traits
 {
