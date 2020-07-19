0a216dcbfb7b3d676846323ab07ce6a6ad9359f1
Maximilien Noal <noal.maximilien@gmail.com>
2020-07-19 17:26:36 +0200

    Avoid needless nofications
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- ViewModels/ToolsVMs/OpenCharacterViewModel.cs
+++ ViewModels/ToolsVMs/OpenCharacterViewModel.cs
@@ -15,9 +15,11 @@ namespace RoleDDNG.ViewModels.ToolsVMs
     {
         private const string DbCharactersQuery = "select nom,image,race,niv_1,niv_2,niv_3,niv_4,niv_5,niv_6,niv_7,niv_8,classe_1,classe_2,classe_3,classe_4,classe_5,classe_6,classe_7,classe_8 from personnage where exclu=false order by nom";
 
+        private ObservableCollection<Personnage> _characters = new ObservableCollection<Personnage>();
+
         private bool _isBusy = false;
 
-        public ObservableCollection<Personnage> Characters { get; private set; } = new ObservableCollection<Personnage>();
+        public ObservableCollection<Personnage> Characters { get => _characters; private set { Set(nameof(Characters), ref _characters, value); } }
 
         public bool IsBusy { get => _isBusy; set { Set(nameof(IsBusy), ref _isBusy, value); } }
 
@@ -28,11 +30,7 @@ namespace RoleDDNG.ViewModels.ToolsVMs
             IsBusy = true;
             var db = new DbAccessor(new Database(SimpleIoc.Default.GetInstance<MainViewModel>().AppSettings.LastCharacterDBPath, DbType.UserCharactersDb));
             var charactersFromDb = await db.GetQueryDataAsync<Personnage>(DbCharactersQuery).ConfigureAwait(true);
-            if (Characters.Any())
-            {
-                Characters.Clear();
-            }
-            charactersFromDb.ToList().ForEach(x => Characters.Add(x));
+            Characters = new ObservableCollection<Personnage>(charactersFromDb);
             IsBusy = false;
         }
     }
