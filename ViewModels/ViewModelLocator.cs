fda2f2aef05cf91d369bf7bda4ba040ef8b6ebbf
Maximilien Noal <noal.maximilien@gmail.com>
2020-07-29 17:33:05 +0200

    Simplified VM Locator
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- ViewModels/ViewModelLocator.cs
+++ ViewModels/ViewModelLocator.cs
@@ -1,18 +1,4 @@
-﻿/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:LTFGameLauncher"
-                           x:Key="Locator" />
-  </Application.Resources>
-
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
-*/
-
-using GalaSoft.MvvmLight.Ioc;
+﻿using GalaSoft.MvvmLight.Ioc;
 using RoleDDNG.ViewModels.Menus.Rules;
 using RoleDDNG.ViewModels.Menus.Tools;
 
@@ -24,20 +10,8 @@ namespace RoleDDNG.ViewModels
     /// </summary>
     public class ViewModelLocator
     {
-        /// <summary> Initializes a new instance of the ViewModelLocator class. </summary>
         public ViewModelLocator()
         {
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
             SimpleIoc.Default.Register(() => new MainViewModel());
             SimpleIoc.Default.Register<DiceRollViewModel>();
             SimpleIoc.Default.Register<TownGeneratorViewModel>();
@@ -50,59 +24,18 @@ namespace RoleDDNG.ViewModels
         // side, where it initializes the SimpleIoc used here
 #pragma warning disable CA1822
 
-        public CharactersXpViewModel CharactersXp
-        {
-            get
-            {
-                return SimpleIoc.Default.GetInstance<CharactersXpViewModel>();
-            }
-        }
+        public CharactersXpViewModel CharactersXp => SimpleIoc.Default.GetInstance<CharactersXpViewModel>();
 
-        public DiceRollViewModel DiceRoll
-        {
-            get
-            {
-                return SimpleIoc.Default.GetInstance<DiceRollViewModel>();
-            }
-        }
+        public DiceRollViewModel DiceRoll => SimpleIoc.Default.GetInstance<DiceRollViewModel>();
 
-        public MainViewModel Main
-        {
-            get
-            {
-                return SimpleIoc.Default.GetInstance<MainViewModel>();
-            }
-        }
+        public MainViewModel Main => SimpleIoc.Default.GetInstance<MainViewModel>();
 
-        public OpenCharacterViewModel OpenCharacter
-        {
-            get
-            {
-                return SimpleIoc.Default.GetInstance<OpenCharacterViewModel>();
-            }
-        }
+        public OpenCharacterViewModel OpenCharacter => SimpleIoc.Default.GetInstance<OpenCharacterViewModel>();
 
-        public RacesDescriptionsViewModel RacesDescriptions
-        {
-            get
-            {
-                return SimpleIoc.Default.GetInstance<RacesDescriptionsViewModel>();
-            }
-        }
+        public RacesDescriptionsViewModel RacesDescriptions => SimpleIoc.Default.GetInstance<RacesDescriptionsViewModel>();
 
-        public TownGeneratorViewModel TownGenerator
-        {
-            get
-            {
-                return SimpleIoc.Default.GetInstance<TownGeneratorViewModel>();
-            }
-        }
+        public TownGeneratorViewModel TownGenerator => SimpleIoc.Default.GetInstance<TownGeneratorViewModel>();
 
 #pragma warning restore CA1822
-
-        internal static void Cleanup()
-        {
-            //Nothing to do here.
-        }
     }
 }
