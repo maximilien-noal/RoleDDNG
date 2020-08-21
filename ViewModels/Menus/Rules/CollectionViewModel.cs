d40363b32545345efecaf1d5e062c119d6b8f0bc
Maximilien Noal <noal.maximilien@gmail.com>
2020-08-21 22:51:20 +0200

    Shared CollectionViewModel base class
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ ViewModels/Menus/Rules/CollectionViewModel.cs
@@ -0,0 +1,67 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace RoleDDNG.ViewModels.Menus.Rules
+{
+    public class CollectionViewModel<T> : ViewModelBase
+        where T : new()
+    {
+        private ObservableCollection<T> _collectionStore = new ObservableCollection<T>();
+
+        private T _selectedItem = new T();
+
+        public CollectionViewModel()
+        {
+            Previous = new RelayCommand(PreviousMethod);
+            Next = new RelayCommand(NextMethod);
+        }
+
+        public ObservableCollection<T> Collection { get => _collectionStore; private set { Set(nameof(Collection), ref _collectionStore, value); } }
+
+        public RelayCommand Next { get; private set; }
+
+        public RelayCommand Previous { get; private set; }
+
+        public T SelectedItem { get => _selectedItem; set { Set(nameof(SelectedItem), ref _selectedItem, value); } }
+
+        protected void NextMethod()
+        {
+            MoveTo(1);
+        }
+
+        protected void PreviousMethod()
+        {
+            MoveTo(-1);
+        }
+
+        protected void SetCollection(ObservableCollection<T> newCollection)
+        {
+            Collection = newCollection;
+        }
+
+        private void MoveTo(int diff)
+        {
+            if (SelectedItem is null)
+            {
+                SelectedItem = Collection.FirstOrDefault();
+                return;
+            }
+            var index = Collection.IndexOf(SelectedItem);
+            if (index != -1)
+            {
+                var newIdex = index += diff;
+                if (newIdex > Collection.Count - 1)
+                {
+                    newIdex = 0;
+                }
+                else if (newIdex < 0)
+                {
+                    newIdex = Collection.Count - 1;
+                }
+                SelectedItem = Collection.ElementAtOrDefault(newIdex);
+            }
+        }
+    }
+}
--- ViewModels/Menus/Rules/RacesDescriptionsViewModel.cs
+++ ViewModels/Menus/Rules/RacesDescriptionsViewModel.cs
@@ -1,7 +1,4 @@
-﻿using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
-
-using RoleDDNG.Models.Roles;
+﻿using RoleDDNG.Models.Roles;
 using RoleDDNG.ViewModels.Interfaces;
 
 using System.Collections.ObjectModel;
@@ -10,73 +7,22 @@ using System.Threading.Tasks;
 
 namespace RoleDDNG.ViewModels.Menus.Rules
 {
-    public class RacesDescriptionsViewModel : ViewModelBase, IDocumentViewModel, IDbDependantViewModel
+    public class RacesDescriptionsViewModel : CollectionViewModel<RacePersonnage>, IDocumentViewModel, IDbDependantViewModel
     {
         private bool _isBusy = false;
 
-        private ObservableCollection<RacePersonnage> _races = new ObservableCollection<RacePersonnage>();
-
-        private RacePersonnage? _selectedRace;
-
-        public RacesDescriptionsViewModel()
-        {
-            Previous = new RelayCommand(PreviousMethod);
-            Next = new RelayCommand(NextMethod);
-        }
-
         public bool IsBusy { get => _isBusy; set { Set(nameof(IsBusy), ref _isBusy, value); } }
-
-        public RelayCommand Next { get; private set; }
-
-        public RelayCommand Previous { get; private set; }
-
-        public ObservableCollection<RacePersonnage> Races { get => _races; private set { Set(nameof(Races), ref _races, value); } }
-
-        public RacePersonnage? SelectedRace { get => _selectedRace; set { Set(nameof(SelectedRace), ref _selectedRace, value); } }
 
         public async Task LoadDbDataAsync()
         {
             IsBusy = true;
             using var progDb = DB.ProgDb.Create();
-            Races = new ObservableCollection<RacePersonnage>(await Task.Run(() => progDb.Query<RacePersonnage>("select race,Description from Race order by race").Where(x => string.IsNullOrWhiteSpace(x.Description) == false && x.Description != x.Race)).ConfigureAwait(false));
-            if (Races.Any())
+            SetCollection(new ObservableCollection<RacePersonnage>(await Task.Run(() => progDb.Query<RacePersonnage>("select race,Description from Race order by race").Where(x => string.IsNullOrWhiteSpace(x.Description) == false && x.Description != x.Race)).ConfigureAwait(false)));
+            if (Collection.Any())
             {
-                SelectedRace = Races.FirstOrDefault();
+                SelectedItem = Collection.FirstOrDefault();
             }
             IsBusy = false;
-        }
-
-        private void GoToWithStep(int diff)
-        {
-            if (SelectedRace is null)
-            {
-                SelectedRace = Races.FirstOrDefault();
-                return;
-            }
-            var index = Races.IndexOf(SelectedRace);
-            if (index != -1)
-            {
-                var newIdex = index += diff;
-                if (newIdex > Races.Count - 1)
-                {
-                    newIdex = 0;
-                }
-                else if (newIdex < 0)
-                {
-                    newIdex = Races.Count - 1;
-                }
-                SelectedRace = Races.ElementAtOrDefault(newIdex);
-            }
-        }
-
-        private void NextMethod()
-        {
-            GoToWithStep(1);
-        }
-
-        private void PreviousMethod()
-        {
-            GoToWithStep(-1);
         }
     }
 }
