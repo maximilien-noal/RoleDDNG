f9ca6ac4fce5467d14c94ad5c98be1bfa05819c7
Maximilien Noal <noal.maximilien@gmail.com>
2021-01-05 00:33:42 +0100

    WIP sessions support
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ Models/Sessions/Session.cs
@@ -0,0 +1,11 @@
+﻿namespace RoleDDNG.Models.Sessions
+{
+    using System.Collections.ObjectModel;
+
+    public class Session
+    {
+        public string Id { get; set; } = "";
+
+        public ObservableCollection<object> ViewModels { get; set; } = new ObservableCollection<object>();
+    }
+}
